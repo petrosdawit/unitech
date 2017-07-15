7d2a597ea758d9ef301a12e42ce6081110f64a8c
petrosdawit <petros_dawit@brown.edu>
2017-07-15 13:21:19 -0700

    more updates on server

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 var anyDB = require('any-db');
-var conn = anyDB.createConnection('sqlite3://nonProfits.db');
+var conn = anyDB.createConnection('sqlite3://data/nonProfits.db');
+
+conn.query('CREATE TABLE IF NOT EXISTS nonProfits (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, ' +
+	'cost TEXT, keyword1 TEXT, keyword2 TEXT, keyword3 TEXT, keyword4 TEXT, keyword5 TEXT);');
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/home.html'));
+  res.sendFile(path.join(__dirname+'/html/home.html'));
 });
